fc2e797830b5aa2adaca43a3535037649b03ff3c
JxstWieslaw <wieslawsamushonga01@gmail.com>
2022-07-24 02:16:06 +0530

    3 files modified, 3 added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import "./App.css";
+import './App.css';
+import './input.css';
 
 function App() {
-  return <div className="App">Wow wow wow</div>;
+  return (
+    <div className="App">
+      <div className="flex md:flex-row flex-col-reverse w-full h-screen">
+        <div className="md:w-1/3 w-full md:h-screen h-1/3">
+          <div className="md:block hidden bg-white h-1/2"></div>
+          <div className="bg-green-500 h-full">1</div>
+        </div>
+        <div className="bg-red-500 h-2/3 w-full md:w-2/3 md:h-screen">2</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
